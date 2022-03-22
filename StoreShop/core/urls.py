be5097d1857e7e3f70daccd9eefb418b910e1172
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-22 14:53:23 +0300

    process payment; increase and decrease good val

--- StoreShop/core/urls.py
+++ StoreShop/core/urls.py
@@ -14,9 +14,12 @@ urlpatterns = [
     path("<str:good_type>", Web.goods, name="goods"),
     path("add_good_to_cart/", Web.add_good_to_cart, name="add_good_to_cart"),
     path("clearance_of_good/<int:order_id>", Web.clearance_of_good, name="clearance_of_good"),
+    path("process_payment", Web.process_payment, name="process_payment"),
 
     path("remove_order_from_cart/", Web.remove_order_from_cart, name="remove_order_from_cart"),
-    path("remove_all_orders_from_cart/", Web.remove_all_orders_from_cart, name="remove_all_orders_from_cart")
+    path("remove_all_orders_from_cart/", Web.remove_all_orders_from_cart, name="remove_all_orders_from_cart"),
+    path("cart_decrease_value/", Web.cart_decrease_value, name="cart_decrease_value"),
+    path("cart_increase_value/", Web.cart_increase_value, name="cart_increase_value")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #handler500 = "core.views.handler500"
