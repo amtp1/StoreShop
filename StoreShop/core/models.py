cf1beea4cc3854d06b6e4b2c7c590d81e14d565f
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-22 14:52:45 +0300

    add good quantity

--- StoreShop/core/models.py
+++ StoreShop/core/models.py
@@ -52,7 +52,6 @@ class Staffs(models.Model):
 
 class Categories(models.Model):
     name=models.CharField(max_length=255, verbose_name='Имя категории')
-    slug=models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
@@ -68,10 +67,10 @@ class Goods(models.Model):
 
     category=models.ForeignKey(Categories, verbose_name='Gategory', on_delete=models.CASCADE)
     title=models.CharField(max_length=255, verbose_name='Title')
-    slug=models.SlugField(unique=True)
     image=models.ImageField(upload_to="images/", verbose_name='Image')
     description=models.TextField(verbose_name='Description', null=True)
     price=models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Price')
+    qty = models.IntegerField(default=0, verbose_name="Quantity")
 
     def __str__(self):
         return self.title
@@ -109,6 +108,8 @@ class Orders(models.Model):
     staff = models.ForeignKey(Staffs, on_delete=models.CASCADE, verbose_name="Staff")
     place_datetime = models.DateTimeField(auto_now_add=True, verbose_name="Place datetime")
     complete_datetime = models.DateTimeField(auto_now_add=True, verbose_name="Complete datetime")
+    qty = models.IntegerField(verbose_name="Quantity")
+    sum = models.FloatField(verbose_name="Sum")
     customer = models.ForeignKey('Customers', on_delete=models.CASCADE, verbose_name="Customer")
 
 
