e4a1c87f384bb1eeff3dee673a2920b20c92d90b
amtp1 <magomedov.abdul20012@gmail.com>
2022-04-05 23:41:47 +0300

    add staff change

--- StoreShop/core/admin.py
+++ StoreShop/core/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from .models import *
 
-@admin.register(Staffs)
-class Staffs(admin.ModelAdmin):
-    fields = ["last_name", "first_name", "position", "birthday", "address", "phone", "note"]
-    list_display = ["last_name", "first_name", "position"]
 
+@admin.register(Staffs)
+class StaffsAdmin(admin.ModelAdmin):
+    fields = ["staff", "is_manager", "is_booker", "is_merchandiser"]
+    list_display = ["staff", "is_manager", "is_booker", "is_merchandiser"]
+
+@receiver(post_save, sender=User)
+def staff_change(instance, sender, *args, **kwargs):
+    if instance.is_staff:
+        staff = Staffs.objects.filter(staff=instance)
+        if not staff.exists():
+            Staffs.objects.create(staff=instance)
+    else:
+        staff = Staffs.objects.filter(staff=instance).delete()
 
 @admin.register(Categories)
 class Categories(admin.ModelAdmin):
